550c13d0ae0e3e5827a5a839bdf1ed508140c463
Алексей Гвоздков <aleksejgvozdkov@MacBook-Air.local>
2025-05-19 01:40:30 +0300

    Fonts
    Created Fonts enum with static font constants

--- WeatherForecast/Helpers/Fonts.swift
+++ WeatherForecast/Helpers/Fonts.swift
@@ -5,3 +5,11 @@
 //  Created by Алексей Гвоздков on 18.05.2025.
 //
 
+import UIKit
+
+enum Fonts {
+    static let title = UIFont.systemFont(ofSize: 32, weight: .bold)
+    static let bigTitle = UIFont.systemFont(ofSize: 60, weight: .medium)
+    static let subtitle = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let body = UIFont.systemFont(ofSize: 14, weight: .regular)
+}
