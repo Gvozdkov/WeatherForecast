9e20c0c28e1f9866ae98bf70c07f578201d0a774
Алексей Гвоздков <aleksejgvozdkov@MacBook-Air.local>
2025-05-19 01:45:19 +0300

    WeatherCollectionView
    - Created reusable WeatherCollectionView
    - Implemented data display for forecast days
    - Configured collection view layout and cells"

--- WeatherForecast/UIComponents/WeatherCollectionView/WeatherCollectionCell.swift
+++ WeatherForecast/UIComponents/WeatherCollectionView/WeatherCollectionCell.swift
@@ -5,3 +5,111 @@
 //  Created by Алексей Гвоздков on 18.05.2025.
 //
 
+import UIKit
+import SnapKit
+
+final class WeatherCollectionCell: UICollectionViewCell {
+    
+    static let reuseID = "WeatherCollectionCell"
+    
+    private lazy var avgtempLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.text = "17"
+        label.font = Fonts.body
+        return label
+    }()
+    
+    private lazy var iconWeatherImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "thermometer.sun")
+        return imageView
+    }()
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.text = "PN"
+        label.font = Fonts.body
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.text = "Описание погоды"
+        label.font = Fonts.body
+        return label
+    }()
+    
+    private lazy var maxwindLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.text = "Скорость ветра 54"
+        label.font = Fonts.body
+        return label
+    }()
+    
+    private lazy var avghumidityLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .left
+        label.text = "Влажность 88"
+        label.font = Fonts.body
+        return label
+    }()
+    
+    private lazy var avgtempStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [avgtempLabel, iconWeatherImage, dateLabel])
+        stackView.axis = .horizontal
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private lazy var infoStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [descriptionLabel, maxwindLabel, avghumidityLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private lazy var weatherStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [avgtempStackView, infoStackView])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        addSubview(weatherStackView)
+
+        weatherStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func configure(with day: ForecastDay) {
+            dateLabel.text = day.date
+            avgtempLabel.text = "\(day.day.avgtempC)°C"
+            descriptionLabel.text = day.day.condition.text
+            maxwindLabel.text = "Ветер: \(day.day.maxwindKph) км/ч"
+            avghumidityLabel.text = "Влажность: \(day.day.avghumidity)%"
+            
+            // Загрузка иконки
+ 
+        }
+}
--- WeatherForecast/UIComponents/WeatherCollectionView/WeatherCollectionView.swift
+++ WeatherForecast/UIComponents/WeatherCollectionView/WeatherCollectionView.swift
@@ -5,3 +5,76 @@
 //  Created by Алексей Гвоздков on 18.05.2025.
 //
 
+import UIKit
+import SnapKit
+
+final class WeatherCollectionView: UIView {
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(WeatherCollectionCell.self, forCellWithReuseIdentifier: WeatherCollectionCell.reuseID)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
+    private var dataSource = [ForecastDay]()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        backgroundColor = .clear
+        
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func configure(data: [ForecastDay]) {
+        dataSource = data
+        collectionView.reloadData()
+    }
+}
+
+extension WeatherCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        dataSource.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: WeatherCollectionCell.reuseID, for: indexPath) as? WeatherCollectionCell else {
+            return UICollectionViewCell()
+        }
+        
+        let dayData = dataSource[indexPath.row]
+        cell.configure(with: dayData)
+        
+        return cell
+    }
+}
+
+extension WeatherCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width - 20
+        let height: CGFloat = 80
+        return CGSize(width: width, height: height)
+    }
+}
+
